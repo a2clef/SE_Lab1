d7e018cc5ce37966dd31cb60ebd74c52a5c046e9
aclef <aclef@qq.com>
2016-11-21 21:32:31 +0800

    added suite

---
+++ src/lab_1/SimplifyTest.java
@@ -0,0 +1,13 @@
+package lab_1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SimplifyTest_C1.class, SimplifyTest_C2.class, SimplifyTest_C3.class, SimplifyTest_C4.class,
+		SimplifyTest_E1.class, SimplifyTest_E2.class, SimplifyTest_E3.class, SimplifyTest_E4.class,
+		SimplifyTest_E5.class, SimplifyTest_Init.class })
+public class SimplifyTest {
+
+}
